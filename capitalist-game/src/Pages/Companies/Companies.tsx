378c123a4ecadd48d9435ba2917a6fd146f1d7b1
Marcin Bigos <marcin.bigos99@gmail.com>
2023-01-16 23:23:08 +0100

    company placeholder

--- capitalist-game/src/Pages/Companies/Companies.tsx
+++ capitalist-game/src/Pages/Companies/Companies.tsx
@@ -1,5 +1,26 @@
+import { DoubleArrow, DriveEta } from "@mui/icons-material";
+import { Avatar, Button, List, ListItemAvatar, ListItemButton, ListItemText, Stack } from "@mui/material";
 import { FC } from "react";
 
-const Companies: FC = () => <>Companies</>;
+const Companies: FC = () => {
+  return (
+    <Stack spacing={3}>
+      <Button variant="contained">Create new</Button>
+      <List dense={false}>
+        <ListItemButton onClick={() => {}}>
+          <ListItemAvatar>
+            <Avatar sx={{ bgcolor: "#3fd970" }}>
+              <DriveEta />
+            </Avatar>
+          </ListItemAvatar>
+          <ListItemText primary="JanuszTrans" secondary={"Delivery company"} />
+          <ListItemAvatar>
+            <DoubleArrow />
+          </ListItemAvatar>
+        </ListItemButton>
+      </List>
+    </Stack>
+  );
+};
 
 export default Companies;
